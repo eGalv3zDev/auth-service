c979da42b0ede6ba7f8a536dc15db35578a3dc24
eGalv3zDev <acamposeco-2024018@kinal.edu.gt>
2026-03-02 11:42:52 -0600

    completar hasta JWT

--- src/AuthService.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/AuthService.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using AuthService.Application.Interfaces;
+using AuthService.Application.Services;
+
 using AuthService.Domain.Entities;
 using AuthService.Domain.Constants;
 using AuthService.Persistence.Data;
@@ -7,16 +10,28 @@ namespace AuthService.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
-    IConfiguration configuration)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
+        // INICIALIZANDO EL CONEXION A LA BASE DE DATOS
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
-                .UseSnakeCaseNamingConvention());
-    
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                       .UseSnakeCaseNamingConvention());
+
+        // Configure application services <------ ACTUALIZACIÓN
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IAuthService, Application.Services.AuthService>();
+        services.AddScoped<IUserManagementService, UserManagementService>();
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+        services.AddScoped<ICloudinaryService, CloudinaryService>();
+        
+        // Servicio de correo
+        services.AddScoped<IEmailService, EmailService>();
+
+
         services.AddHealthChecks();
 
         return services;
     }
 }
-
